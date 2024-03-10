037bb8ad42fbd2c974a84eaffdcb6c52dee147d6
Gensokyo <liuweixing@pcitech.com>
2024-03-10 09:41:52 +0800

    feat：重构数据生成模块

--- src/main/java/org/gensokyo/data/constant/ValueSelectStrategyType.java
+++ src/main/java/org/gensokyo/data/constant/ValueSelectStrategyType.java
@@ -1,18 +1,18 @@
 /*
- * Copyright © 2021 PCI Technology Group Co.,Ltd. All Rights Reserved.
+ * Copyright © 2024 PCI Technology Group Co.,Ltd. All Rights Reserved.
  * Site: http://www.pcitech.com/
  * Address：PCI Intelligent Building, No.2 Xincen Fourth Road, Tianhe District, Guangzhou，China（Zip code：510653）
  */
 package org.gensokyo.data.constant;
 
 /**
- * 任务状态枚举
+ * 选择策略类型
  *
  * @author Gensokyo V.L.
  * @version 1.0.0
- * @since 2023/1/29 , Version 1.0.0
+ * @since 2023/10/26 , Version 1.0.0
  */
-public enum TaskStatus {
+public enum ValueSelectStrategyType {
 
-    INIT, STARTED, PROCESSING, COMPLETE, ERROR
+    REPEAT_RANDOM, REPEAT_ORDER, ONCE_RANDOM, ONCE_ORDER, MULTIPLE_ORDER
 }
