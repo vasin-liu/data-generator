4f585f50a9ab8c85dedac4f5b36fc28ef259da26
Gensokyo <liuweixing@pcitech.com>
2026-05-08 13:44:16 +0800

    feat: add v2 execution and transformer metadata models

---
+++ data-generator-common/data-generator-core/src/main/java/org/gensokyo/data/model/v2/ExecutionPolicyVO.java
@@ -0,0 +1,17 @@
+package org.gensokyo.data.model.v2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class ExecutionPolicyVO implements Serializable {
+    private String mode;
+    private Integer maxRowsInMemory;
+    private Integer previewRowLimit;
+    private Integer sourceChunkSize;
+    private Integer sinkBatchSize;
+    private Boolean failOnLimitExceeded;
+}
